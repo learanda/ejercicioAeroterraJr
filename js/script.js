d33105fa82809045fae90cc68fbe036f044bc393
Leandro Aranda <learanda@hotmail.com.ar>
2021-01-23 22:55:03 -0300

    modifico zoom del map

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function initMap() {
     var aeroterra = { lat: -34.59555708657061, lng: -58.37094880287956 };
 
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
+        zoom: 15,
         center: aeroterra,
       });
 
