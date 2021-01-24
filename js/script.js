482663c0e251a58f137d5620ae2ad3a49a78e495
Leandro Aranda <learanda@hotmail.com.ar>
2021-01-23 22:48:53 -0300

    cree archivo js y comence a usar la api cargando un mapa con la funcion initMap

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function initMap() {
+
+    var aeroterra = { lat: -34.59555708657061, lng: -58.37094880287956 };
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: aeroterra,
+      });
+
+    var marker = new google.maps.Marker({
+    position: aeroterra,
+    map: map,
+    });
+}
