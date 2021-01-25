a498472e46a8b5bc5e7f5d17bd32f6482b0dabe1
Leandro Aranda <learanda@hotmail.com.ar>
2021-01-25 15:43:30 -0300

    modifico funcion initMap para que inicie un mapa base sin marcadores

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 function initMap() {
 
-    var aeroterra = { lat: -34.59555708657061, lng: -58.37094880287956 };
+    var mapaBase = { lat: -34.6020549, lng: -58.3818864 };
 
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 15,
-        center: aeroterra,
+        center: mapaBase,
       });
 
-    var marker = new google.maps.Marker({
-    position: aeroterra,
+    var posicionBase = new google.maps.Map({
+    position: mapaBase,
     map: map,
     });
 
